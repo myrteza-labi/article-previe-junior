d5440c3360e603e14d8853fc5db9cab6004f9a2e
Myrteza <myrteza@hotmail.fr>
2021-10-13 23:43:51 +0200

    :style: (footer) all footers styles done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,78 +7,145 @@ import ShareBox from './ShareBox';
 import shareBtnStatic from '../images/icon-share.svg'; 
 import shareBtnActive from '../images/icon-share-active.svg'; 
 import profilepicture from '../images/avatar-michelle.jpg';
-import IconContainer from './IconContainer';
 
 
 
+    
+    
+    class Footer extends React.Component{
+        constructor(props){
+            super(props); 
+            this.state = {
+                renderedFooter: this.footerStatic,
+                className: "Footer footerMobileStatic",
+                isStatic: true,
+                isDesktop : (window.innerWidth >= 1440 ? true: false) ,
+            }
+            this.handleClick = this.handleClick.bind(this)
+            this.isStatic = this.isStatic.bind(this); 
+            this.displayDesktopFooterActive = this.displayDesktopFooterActive.bind(this); 
+            this.displayFooterStatic = this.displayFooterStatic.bind(this); 
+            this.displayMobileFooterActiv = this.displayMobileFooterActiv.bind(this); 
+            this.limitFunc = this.limitFunc.bind(this); 
 
-class Footer extends React.Component{
-    constructor(props){
-        super(props); 
-        this.state = {
-            renderedFooter: "",
+
+    }
+    
+    limitFunc(){
+        let isStatic = this.state.isStatic;
+        let isDesktop = this.state.isDesktop; 
+        let className= this.state.className; 
+        if(window.innerWidth >= 1440){
+            this.setState({
+                isDesktop : true,
+            })
+        }
+        else{
+            this.setState({
+                isDesktop : false,
+            })
+        }
+    }
+/*
+        if(isStatic && isDesktop){
+            if(className == !"Footer footerMobileStatic"){
+                this.displayFooterStatic();
+            }
+        }
+        else if (!isStatic && isDesktop){
+            if(className == !"Footer footerDesktopActive"){
+                this.displayDesktopFooterActive()
+            }
+        } else if (isStatic && !isDesktop){
+            if(className == "Footer footerMobileStatic"){
+                this.displayFooterStatic()
+            }
+        } else if(!isStatic && !isDesktop){
+            if(className == !"Footer footerMobileActive"){
+                this.displayMobileFooterActiv()
+            }
+        }
+    }
+    */
+
+
+    isStatic(){
+        if(this.state.isStatic === true){
+            return true
+        }
+    }
+
+    displayFooterStatic(){
+        this.setState({
+            renderedFooter : this.footerStatic,
+            className: "Footer footerMobileStatic",
+            isStatic: true,
+        })
+    }
+
+    displayDesktopFooterActive(){
+        this.setState({
+            renderedFooter : this.desktopFooterActive,
             className: "Footer footerDesktopActive",
-            windowWidth: window.innerWidth,
+            isStatic: false,
+        })
+    }
+
+    displayMobileFooterActiv(){
+        this.setState({
+            renderedFooter : this.mobileFooterActive,
+            className: "Footer footerMobileActive",
+            isStatic: false,
+        })
+    }
+
+    handleClick(){
+        let isStatic = this.state.isStatic;
+        let isDesktop = this.state.isDesktop; 
+
+        if( isStatic && !isDesktop){
+            this.displayMobileFooterActiv(); 
+        }
+        else if (!isStatic && !isDesktop){
+            this.displayFooterStatic();
+        }
+        else if(isStatic && isDesktop){
+            this.displayDesktopFooterActive(); 
+        }
+        else if(!isStatic && isDesktop){
+            this.displayFooterStatic();
         }
     }
     
- 
+
+    footerStatic = 
+
+    <div className="footerMainContainer footerMainContainerStatic" >
+            <ProfileBox srcProfilePicture={profilepicture} 
+                        altProfilePicture={"Michelle Appleton"}
+                        name={"Michelle Appleton"}
+                        date={"28 juin 2020"}/>
+
+            <ShareBtn   src={shareBtnStatic} 
+                        onClick={()=> this.handleClick()}
+                        ContainerClassName={"shareBtnContainer shareBtnContainerStatic"} 
+                        shareBtnClassName={"shareBtn shareBtnMobileStatic"}/>
+    </div>
 
 
-    render(){
-        let renderedFooter; 
-
-        let mobileFooterStatic = 
-
-        <div className="footerMainContainer footerMainContainerStatic" >
-                <ProfileBox srcProfilePicture={profilepicture} 
-                            altProfilePicture={"Michelle Appleton"}
-                            name={"Michelle Appleton"}
-                            date={"28 juin 2020"}/>
-
-                <ShareBtn   src={shareBtnStatic} 
-                            ContainerClassName={"shareBtnContainer shareBtnContainerStatic"} 
-                            shareBtnClassName={"shareBtn shareBtnMobileStatic"}/>
-        </div>
-
-
-
-        let mobileFooterActive = 
+    mobileFooterActive = 
             <div className="footerMainContainer footerMainContainerActive">
                 <ShareBox   className="ShareBox ShareBoxMobile" 
                             shareWord={"SHARE"} />
 
                 <ShareBtn   ContainerClassName={"shareBtnContainer shareBtnContainerActive"}
+                            onClick={()=> this.handleClick()}
                             shareBtnClassName={"shareBtn shareBtnMobileActive"}
                             src={shareBtnActive}/>
             </div>
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        let desktopFooterActive= 
+    desktopFooterActive= 
         <div className="footerMainContainer footerMainContainerDesktopActive">
             <ProfileBox srcProfilePicture={profilepicture} 
                             altProfilePicture={"Michelle Appleton"}
@@ -86,6 +153,7 @@ class Footer extends React.Component{
                             date={"28 juin 2020"}/>
 
             <ShareBtn   ContainerClassName={"shareBtnContainer shareBtnContainerActive"}
+                        onClick={()=> this.handleClick()}
                         shareBtnClassName={"shareBtn shareBtnDesktopActive"}
                         src={shareBtnActive}/>
 
@@ -94,11 +162,18 @@ class Footer extends React.Component{
 
             
         </div>
+
+
+    render(){
+        
+        window.addEventListener("resize", this.limitFunc);
+        window.addEventListener("onload", this.limitFunc);
+        
             
 
         return(
             <section id="Footer" className={this.state.className} >
-                {desktopFooterActive}
+                {this.state.renderedFooter}
             </section>
         )
     }
@@ -109,47 +184,3 @@ class Footer extends React.Component{
 }
 
 export default Footer; 
-
-
-/*
-
-
-
-
-
-
-                {this.state.className == "Footer active"
-                ?
-                <div className="mainFooterContainer mainFooterContainerActive">
-                    <p className="shareWord" >SHARE</p>
-                        {this.props.icon}
-                        
-                        <div className="shareContainer shareContaineractive " >
-                            <img src={shareIconActive} 
-                            alt={"une fleche indiquant le partage vers d'autre plateforme"} 
-                            className="shareIcon shareIconactive"/>
-                        </div>
-                    
-                </div>
-                :   
-                <div className="mainFooterContainer mainFooterContainerStatic">
-                    <img className="profilePicture" src={this.props.profilePictureSrc} />
-                    <div className="nameAndDateContainer" >
-                        <p className="name">{this.props.name}</p>
-                        <p className="date" >{this.props.date}</p>
-                    </div>
-                    <div className="shareContainer shareContainerStatic " >
-                        <img src={shareIcon} 
-                        alt={"une fleche indiquant le partage vers d'autre plateforme"} 
-                        className="shareIcon"/> 
-                    </div>
-                    
-                </div>
-                }
-
-
-
-
-
-
-*/
--- src/components/ShareBtn.js
+++ src/components/ShareBtn.js
@@ -2,7 +2,7 @@ import './ShareBtn.css';
 
 function ShareBtn(props){
     return(
-        <div className={props.ContainerClassName} >
+        <div onClick={props.onClick} className={props.ContainerClassName} >
             <img src={props.src} 
                  alt={"une fleche indiquant le partage vers d'autres plateformes"} 
                  className={props.shareBtnClassName}/>
