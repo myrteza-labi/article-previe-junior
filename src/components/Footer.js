c28f18522f39679754154ea6d78502f514f371d9
Myrteza <myrteza@hotmail.fr>
2021-10-13 13:01:58 +0200

    :mobile-style: (footer) active style done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Footer.css'; 
 import './IconContainer'; 
+import shareIcon from '../images/icon-share.svg'; 
+import shareIconActive from '../images/icon-share-active.svg'; 
+
 
 class Footer extends React.Component{
     constructor(props){
@@ -21,9 +24,15 @@ class Footer extends React.Component{
                 {this.state.className == "Footer active"
                 ?
                 <div className="mainFooterContainer">
-                    <p className="shareWord" ></p>
+                    <p className="shareWord" >SHARE</p>
                         {this.props.icon}
-                    <div className="shareIcon active" ></div>
+                        
+                        <div className="shareContainer shareContaineractive " >
+                            <img src={shareIconActive} 
+                            alt={"une fleche indiquant le partage vers d'autre plateforme"} 
+                            className="shareIcon shareIconactive"/>
+                        </div>
+                    
                 </div>
                 :   
                 <div className="mainFooterContainer">
@@ -32,7 +41,12 @@ class Footer extends React.Component{
                         <p className="name">{this.props.name}</p>
                         <p className="date" >{this.props.date}</p>
                     </div>
-                    <div className="shareIcon" ></div> 
+                    <div className="shareContainer shareContainerStatic " >
+                        <img src={shareIcon} 
+                        alt={"une fleche indiquant le partage vers d'autre plateforme"} 
+                        className="shareIcon"/> 
+                    </div>
+                    
                 </div>
                 }
 
